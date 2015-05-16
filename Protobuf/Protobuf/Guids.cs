dd1b6a46409e4b5b0c84aac25d4c8e4f01cb524f
Michael Reukauff <michael@reukauff.de>
2015-05-16 23:32:40 +0200

    PROTBUF-25 added vspackage files

---
+++ Protobuf/Protobuf/Guids.cs
@@ -0,0 +1,15 @@
+﻿// Guids.cs
+// MUST match guids.h
+using System;
+
+
+namespace MichaelReukauff.Protobuf
+{
+    static class GuidList
+    {
+        public const string guidProtobufPkgString = "5d89ec18-2175-4dc0-861a-9718edfa0be1";
+        public const string guidProtobufCmdSetString = "5048df81-256d-4c07-bfe0-ebd0c4cfa657";
+
+        public static readonly Guid guidProtobufCmdSet = new Guid(guidProtobufCmdSetString);
+    };
+}
---
+++ Protobuf/Protobuf/ProtobufPackage.cs
@@ -0,0 +1,200 @@
+﻿#region Copyright © 2014 Michael Reukauff
+// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ProtobufPackage.cs" company="Michael Reukauff">
+//   Copyright © 2015 Michael Reukauff
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+#endregion
+
+namespace MichaelReukauff.Protobuf
+{
+    using System;
+    using System.Diagnostics;
+    using System.Globalization;
+    using System.IO;
+    using System.Runtime.InteropServices;
+    using System.Text;
+
+    using EnvDTE;
+
+    using Microsoft.VisualStudio;
+    using Microsoft.VisualStudio.Shell;
+    using Microsoft.VisualStudio.Shell.Interop;
+
+    using Constants = EnvDTE.Constants;
+
+    /// <summary>
+    /// This is the class that implements the package exposed by this assembly.
+    /// <para>
+    /// The minimum requirement for a class to be considered a valid package for Visual Studio
+    /// is to implement the IVsPackage interface and register itself with the shell.
+    /// This package uses the helper classes defined inside the Managed Package Framework (MPF)
+    /// to do it: it derives from the Package class that provides the implementation of the 
+    /// IVsPackage interface and uses the registration attributes defined in the framework to 
+    /// register itself and its components with the shell.
+    /// </para>
+    /// </summary>
+    //// This attribute tells the PkgDef creation utility (CreatePkgDef.exe) that this class is a package.
+    [PackageRegistration(UseManagedResourcesOnly = true)]
+
+    // This attribute is used to register the information needed to show this package
+    // in the Help/About dialog of Visual Studio.
+    [InstalledProductRegistration("#110", "#112", "1.0", IconResourceID = 400)]
+
+    // guid of this package
+    [Guid(GuidList.guidProtobufPkgString)]
+
+    // load this package even if no solution or project is loaded
+    [ProvideAutoLoad(VSConstants.UICONTEXT.NoSolution_string)]
+
+    // ReSharper disable once InconsistentNaming
+    public sealed class ProtobufPackage : Package
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ProtobufPackage"/> class. 
+        /// Default constructor of the package.
+        /// Inside this method you can place any initialization code that does not require 
+        /// any Visual Studio service because at this point the package object is created but 
+        /// not sited yet inside Visual Studio environment. The place to do all the other 
+        /// initialization is the Initialize method.
+        /// </summary>
+        public ProtobufPackage()
+        {
+            var line = string.Format(CultureInfo.CurrentCulture, "Entering constructor for: {0}", ToString());
+            Debug.WriteLine(line);
+        }
+
+        /////////////////////////////////////////////////////////////////////////////
+        // Overridden Package Implementation
+        /////////////////////////////////////////////////////////////////////////////
+
+        #region Package Members
+        /// <summary>
+        /// Initialization of the package; this method is called right after the package is sited, so this is the place
+        /// where you can put all the initialization code that rely on services provided by VisualStudio.
+        /// </summary>
+        protected override void Initialize()
+        {
+            Debug.WriteLine(string.Format(CultureInfo.CurrentCulture, "Entering Initialize() of: {0}", ToString()));
+            base.Initialize();
+
+            SetupEvents();
+        }
+        #endregion Package Members
+
+        #region private methods
+        /// <summary>
+        /// Set up the events we wnt to handle.
+        /// </summary>
+        private void SetupEvents()
+        {
+            var dte = (DTE)GetService(typeof(SDTE));
+            var dteEvents = dte.Events;
+            var documentEvents = dteEvents.DocumentEvents;
+            documentEvents.DocumentSaved += OnDocumentSaved;
+        }
+
+        /// <summary>
+        /// Event handler when a decument was saved.
+        /// </summary>
+        /// <param name="document">The document.</param>
+        private void OnDocumentSaved(Document document)
+        {
+            var path = document.FullName;
+            var ext = Path.GetExtension(path);
+            if (ext == null || ext.ToLowerInvariant() != ".proto")
+            {
+                return;
+            }
+
+            SaveProtoWithoutBom(path, document);
+
+            ////var file = path + ".cs";
+
+            //// remove all dependent files
+            ////while (document.ProjectItem.ProjectItems.Count > 0)
+            ////{
+            ////    document.ProjectItem.ProjectItems.Item(1).Delete();
+            ////}
+
+            ////using (var str = new StreamWriter(file))
+            ////{
+            ////    str.WriteLine("//-----------------------------------------------------------------------------");
+            ////    str.WriteLine("// <auto-generated>");
+            ////    str.WriteLine("//     This code was generated by a tool.");
+            ////    str.WriteLine("//");
+            ////    str.WriteLine("//     Changes to this file may cause incorrect behavior and will be lost if");
+            ////    str.WriteLine("//     the code is regenerated.");
+            ////    str.WriteLine("// </auto-generated>");
+            ////    str.WriteLine("//------------------------------------------------------------------------------");
+            ////}
+
+            ////document.ProjectItem.ProjectItems.AddFromFile(file);
+        }
+
+        /// <summary>
+        /// Save the file without a bom.
+        /// </summary>
+        /// <param name="file">The filename.</param>
+        /// <param name="doc">The document.</param>
+        private void SaveProtoWithoutBom(string file, Document doc)
+        {
+            byte[] bytes;
+
+            // read the first three byte to see if a byte order mark (BOM) is set
+            using (BinaryReader reader = new BinaryReader(File.Open(file, FileMode.Open)))
+            {
+                ////bytes = reader.ReadBytes((int)reader.BaseStream.Length);
+                bytes = reader.ReadBytes(3);
+            }
+
+            if (bytes[0] != 0xef || bytes[1] != 0xbb || bytes[2] != 0xbf)
+            {
+                return;
+            }
+
+            foreach (Project project in doc.DTE.Solution.Projects)
+            {
+                foreach (ProjectItem item in project.ProjectItems)
+                {
+                    Debug.WriteLine(item.Name);
+                    var projfile = (string)item.Properties.Item("FullPath").Value;
+
+                    if (string.Equals(projfile, file, StringComparison.CurrentCultureIgnoreCase))
+                    {
+                        doc.Close(vsSaveChanges.vsSaveChangesYes);
+
+                        item.Remove();
+
+                        ////using (var writer = new BinaryWriter(File.Open(file, FileMode.Truncate)))
+                        ////{
+                        ////    writer.Write(bytes.Skip(3).ToArray());
+                        ////}
+
+                        var text = File.ReadAllText(file);
+
+                        File.Delete(file);
+
+                        using (var stream = new StreamWriter(file, false, new UTF8Encoding(false)))
+                        {
+                            stream.Write(text);
+                            stream.Flush();
+                        }
+
+                        project.ProjectItems.AddFromFile(file);
+                        var win = doc.DTE.OpenFile(Constants.vsViewKindCode, file);
+                        win.Activate();
+
+                        break;
+                    }
+
+                    ////foreach (Property property in item.Properties)
+                    ////{
+                    ////    Debug.WriteLine("  " + property.Name + " - " + property.Value);
+                    ////}
+                }
+            }
+        }
+        #endregion private methods
+    }
+}
