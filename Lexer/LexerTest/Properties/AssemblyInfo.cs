4e56663e882af8123ddacffd6597dcd14a3d823c
Michael Reukauff <michael@reukauff.de>
2015-08-08 21:52:14 +0200

    Version 1.7.4 fixed install error

--- Lexer/LexerTest/Properties/AssemblyInfo.cs
+++ Lexer/LexerTest/Properties/AssemblyInfo.cs
@@ -40,4 +40,4 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.7.*")]
-[assembly: AssemblyFileVersion("1.7.1.0")]
+[assembly: AssemblyFileVersion("1.7.4.0")]
