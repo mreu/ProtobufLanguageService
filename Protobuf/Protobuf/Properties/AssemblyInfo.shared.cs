dde18ab752e47bbdecb1aa83a796aebd8928b36e
Michael Reukauff <michael@reukauff.de>
2017-04-04 22:35:41 +0200

    PROTOBUF-30 Added VS2017 support

--- Protobuf/Protobuf/Properties/AssemblyInfo.shared.cs
+++ Protobuf/Protobuf/Properties/AssemblyInfo.shared.cs
@@ -20,5 +20,5 @@ using System.Reflection;
 //
 // You can specify all the values or you can default the Revision and Build Numbers
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.9.*")]
-[assembly: AssemblyFileVersion("1.9.0.0")]
+[assembly: AssemblyVersion("1.10.*")]
+[assembly: AssemblyFileVersion("1.10.0.0")]
